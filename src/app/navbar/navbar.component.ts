848879d400d2bffb6e4b477c5eb83e56ca128504
himanshu pratap <himanshupratap40@gmail.com>
2020-10-05 23:43:11 -0700

    navbar open close done

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -6,6 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
+  onSave(){    
+ 
+    document.getElementById("navbarSupportedContent").classList.add("mystyle")
+    document.getElementById("navbarSupportedContent").style.transition="2s";
+  } 
+  Shownavbar(){
+   
+    document.getElementById("navbarSupportedContent").style.transition="1s";
+    document.getElementById("navbarSupportedContent").classList.remove("mystyle")
+    document.getElementById("navbarSupportedContent").classList.remove("show")
+
+
+  }
 
   constructor() { }
 
